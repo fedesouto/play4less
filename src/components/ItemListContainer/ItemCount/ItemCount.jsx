dac2366839a4449236d891850cf57e4165656ef3
Federico <federicogsouto@gmail.com>
2021-11-09 21:15:26 -0300

    ItemCount desarrollado

--- src/components/ItemListContainer/ItemCount/ItemCount.jsx
+++ src/components/ItemListContainer/ItemCount/ItemCount.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsPlusLg, BsDashLg } from 'react-icons/bs'
-import { Container, InputGroup, FormControl, Button } from 'react-bootstrap';
+import { Container, InputGroup, FormControl , Button } from 'react-bootstrap';
 
 const ItemCount = ({ initial, stock, onAdd }) => {
-
+    const [selectedItems, setSelectedItems] = useState(initial);
+    
+    const handlePlus = () =>{
+            if(selectedItems < stock){
+                setSelectedItems(selectedItems+1);
+            }
+            else{
+                alert(`Hay ${stock} productos disponibles.`)
+            } 
+    }
+    const handleDash = () => {
+            if(selectedItems > 1) {
+                setSelectedItems(selectedItems-1);        
+    }}
+    
     return(
         <Container>
                 <InputGroup className="itemCounter">
-                    <Button className="mainColorButton">
+                    <Button className="mainColorButton" onClick={handleDash}>
                         <BsDashLg />
                     </Button>
-                    <FormControl type="number" value={initial} className="text-center"/>
-                    <Button className="mainColorButton">
+                    <FormControl className="itemCounter" value={selectedItems} />
+                    <Button className="mainColorButton" onClick={handlePlus}>
                         <BsPlusLg />
                     </Button>
                 </InputGroup>
-                    <Button className="my-2 mainColorButton" onClick={onAdd}>
+                    <Button className="my-2 mainColorButton" onClick={() => onAdd(selectedItems)}>
                          Agregar al carrito
                     </Button>
         </Container>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
 import ItemCount from './ItemCount/ItemCount';
 const photos = require('./photosPlaceholder.json');
 
 function ItemListContainer ({name}) {
-          
+    
+        const [stock, setStock] = useState(10);
+        const [cartItems, setCartItems] = useState(0)
+
+        const handleAdd = (orderedItems) => {
+            if(orderedItems <= stock) {
+                setStock(stock - orderedItems)
+                setCartItems(cartItems + orderedItems)
+                alert(`Se agregaron ${orderedItems} productos al carrito.`) 
+                }
+            else if(stock === 0){
+                alert('No hay productos disponibles.')
+            }
+            else alert(`Solo hay ${stock} productos disponibles.`);
+        }
+
         return ( 
             <div className="container-fluid pt-3 text-center">
                 <h2 className="display-5">{name ? `¡${name}, bienvenido  a Play4Less!` : "Inicia sesión para continuar."}</h2>
                 <div className="container-fluid flex-wrap catalogo mt-3">
-                    {photos.map(({url, id, title}) => {
-                        return(
-                        <div className="d-flex flex-column" key={id}>
-                            <img src={url} className="imgCatalogo" alt={title}/>
-                            <strong>{id}</strong>
-                            <ItemCount initial="1"/>
-                        </div>)})}
+                    
+                        <div className="d-flex flex-column" key={photos[0].id}>
+                            <img src={photos[0].url} className="imgCatalogo" alt={photos[0].title}/>
+                            <strong>{photos[0].id}</strong>
+                            <ItemCount initial={1} stock={stock} onAdd={handleAdd}/>
+                        </div>
                 </div>
             </div>
          );
